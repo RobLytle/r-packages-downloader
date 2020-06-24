7c3445f30262c36bcb5a6e7153e75e2934a3e267
Robert Lytle <ral14j@my.fsu.edu>
2020-06-24 15:20:43 -0400

    initialize

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Repo to make it easy to update/re-install my installed packages when updating R/on new machines.
+
+### Adapted from [@carlislerainey](https://github.com/pos5737/r-packages)
+
---
+++ get-pkgs.R
@@ -0,0 +1,6 @@
+# create a .csv of installed packages
+library(tidyverse)
+packages <- data.frame(installed.packages(), stringsAsFactors = FALSE) %>%
+	select(package = Package, version = Version) %>%
+	write_csv("pkgs.csv") %>%
+	glimpse()
